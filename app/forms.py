09f073db8fe110191f70ebf30d5ac43482671c45
Prajapati__Hardik__24 <hardikprajapati242008@gmail.com>
2026-07-22 19:15:27 +0530

    Changes :- Log in System

--- app/forms.py
+++ app/forms.py
@@ -2,11 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms  import StringField , PasswordField , SubmitField , BooleanField
 from wtforms.validators import DataRequired , Length , Email , EqualTo , ValidationError
 from app.models import User
-from app import login_manager
-
-@login_manager.user_loader
-def load_user(user_id):
-     return User.query.get(int(user_id))
 
 
 class registration(FlaskForm):   # it is inher. Flask form 
@@ -42,5 +37,5 @@ class registration(FlaskForm):   # it is inher. Flask form
 class LoginForm(FlaskForm):
     email = StringField("UserEmail",validators=[DataRequired(),Email()])
     password = PasswordField("Password",validators=[DataRequired(),Length(min=8 , max= 20)])
-    remember = BooleanField("RM")
+    remember = BooleanField("Remember Me")
     submit = SubmitField("Log In ")
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,8 @@ from flask import render_template , flash , url_for , redirect
 from app.forms import registration, LoginForm
 from app.models import  User ,Post
 from flask import request
+from flask_login import login_user , current_user , logout_user
+
 #---------------------------------------------
 #this is a dummy data for testing 
 post = [
@@ -35,11 +37,26 @@ def contact():
 
 @app.route("/login",methods=["POST","GET"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data)
+            flash(f"Log in ","success")
+            return redirect(url_for('home'))
+        else:
+            flash("Invalid email or password.", "danger")
+    # elif request.method == "POST":
+    #     flash("Invalid credentials", "danger")
+
     return render_template("login.html", title = "Log In " ,form = form)
 
 @app.route("/register",methods=["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+            return redirect(url_for('home'))
     form = registration()   
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -52,3 +69,12 @@ def register():
         flash("Something went wrong while creating your account.", "danger")
  
     return render_template("register.html",title = "Registration",form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+def account():
+    return render_template("account.html",title = "Profile | My Account")
